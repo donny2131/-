20af34e2e6d48e5bbe26dc7d9b1ea102fa0a4211
donny2131 <55747762+donny2131@users.noreply.github.com>
2020-01-14 18:52:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,12 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-client["on"]('message', message => {
-if(message["author"]["bot"]) return undefined;
-let args = message["content"]["split"](" ");
-if(args[0]["toLowerCase"]() == prefix + `start`) {
-message["channel"]["send"](`**#rep <@256646248931655681>**`)
+
+client.on('message', message => {
+if(message.author.bot) return;
+let args = message.content.split(" ");
+     if (message.content === "start") {
+message.channel.send(`**#rep <@256646248931655681>**`)
+}
 });
 
 

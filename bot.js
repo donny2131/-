2869d90a155f3238f2480280f262e798dec845e7
donny2131 <55747762+donny2131@users.noreply.github.com>
2020-01-14 18:31:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,4 +6,10 @@ let args = message["content"]["split"](" ");
 if(args[0]["toLowerCase"]() == prefix + `start`) {
 message["channel"]["send"](`**#rep <@256646248931655681>**`)
 });
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
